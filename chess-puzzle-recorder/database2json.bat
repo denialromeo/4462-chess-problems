abc89042017fe1df9671b4338667275affa69e35
Daniel Moore <hi@danielmoore.us>
2019-03-07 16:51:14 -0800

    Added mechanism for simplifying output JSON.

--- chess-puzzle-recorder/database2json.bat
+++ chess-puzzle-recorder/database2json.bat
@@ -1,3 +1,5 @@
 @echo { "moves": > problems.json
 @node -e "const{printAllData}=require('./database.js');printAllData();" >> problems.json
 @echo } >> problems.json
+@node simplify-json > ..\chess-puzzle-player\problems.json
+@del problems.json
---
+++ chess-puzzle-recorder/simplify-json.js
@@ -0,0 +1,19 @@
+const movesJson = require("./problems.json").moves
+
+var moves = []
+var builder = {}
+for (let i = 0; i < movesJson.length; i++) {
+    let move = Object.assign({}, movesJson[i])
+    if (move.problemid !== builder.problemid) {
+        moves.push(builder)
+        builder = Object.assign({}, move)
+        delete builder.fen
+        delete builder.move
+        builder.fens = [move.fen]
+    }
+    else {
+        builder.fens.push(move.fen)
+    }
+}
+moves.push(builder)
+console.log(JSON.stringify({"moves": moves.slice(1)}))
