927f053c458ec64db684485f2e09a1cfeabd3d91
Daniel Moore <hi@danielmoore.us>
2019-03-29 10:36:35 -0700

    Puzzle player now displays random solved problem and allows play-through.

--- chess-puzzle-player/display.js
+++ chess-puzzle-player/display.js
@@ -1,15 +1,17 @@
 const { ChessBoard } = require("./chessboard/chessboard.js")
 const { problems } = require("./problems.json")
+const random = require("./random.js")
 
 function init() {
+    const problem = random.choice(problems)
+    const moves = problem.moves.split(";")
     const board = ChessBoard('board', {
         draggable: true,
-        position: problems[0].fen,
+        position: problem.fen,
         dropOffBoard: 'snapback',
     });
 
-    document.querySelector("#startBtn").onclick = board.start
-    document.querySelector("#clearBtn").onclick = board.clear
+    document.querySelector("#startBtn").onclick = function() { board.move(moves[0]); moves.shift() }
 }
 
 // Exports
--- chess-puzzle-recorder/output-json.bat
+++ chess-puzzle-recorder/output-json.bat
@@ -1,4 +1,4 @@
 @echo { "problems": > problems.json
 @node -e "const{printAllData}=require('./database.js');printAllData();" >> problems.json
 @echo } >> problems.json
-@move problems.json ..\chess-puzzle-player\problems.json
+@move /y problems.json ..\chess-puzzle-player\problems.json
