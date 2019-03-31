f77ce52171c5c626dd8413af3ab9aa74e58bdcbc
Daniel Moore <hi@danielmoore.us>
2019-03-31 10:00:04 -0700

    Solved a few more problems. JSON intermediate file more annoying than I would like. Have to work out connecting database to view.

--- chess-puzzle-recorder/app.js
+++ chess-puzzle-recorder/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const request = require('request')
+const path = require('path')
+const { exec } = require('child_process')
 
 const { commitProblem } = require('./database.js')
 
@@ -11,6 +13,7 @@ app.use("/static", express.static(__dirname + '/static'))
 app.use(bodyparser.urlencoded({ extended: true }))
 
 app.get('/', function(req, res) {
+    delete require.cache[path.resolve('./unsolved.json')]
     const { unsolved } = require('./unsolved.json')
     res.render("chess-puzzle-recorder", unsolved[0])
 })
@@ -19,6 +22,7 @@ app.post('/', function(req, res) {
     var { problemid, first, type, fen, moves } = req.body
     console.log(problemid, type, first, fen, moves)
     commitProblem(problemid, type, first, fen, moves, 1)
+    exec("output-recorder-json", (err,stdout,stderr)=> { console.log(stdout) })
     request("http://localhost:8000", function(req,res,next){})
 })
 
