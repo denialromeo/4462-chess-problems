f777bf703f5af34d57053b2baca50ffa8e56813d
Daniel Moore <hi@danielmoore.us>
2019-04-04 22:44:29 -0700

    I'm such a goober. Just realized chess engines exist. This project just became so much easier.

--- polgar.py
+++ polgar.py
@@ -1,10 +1,24 @@
+import chess
+import chess.engine
 from chess.pgn import read_game
+
+engine = chess.engine.SimpleEngine.popen_uci("stockfish.exe")
+
+def solve(fen):
+    moves = []
+    board = chess.Board(fen)
+    while not board.is_game_over():
+        result = engine.play(board, chess.engine.Limit(time=0.100))
+        moves.append(str(result.move)[0:2] + "-" + str(result.move)[2:])
+        board.push(result.move)
+    return str.join(";", moves)
 
 def d():
   f = open("polgar.pgn")
   g = read_game(f)
-  for i in range(2000):
+  for i in range(310):
     g = read_game(f)
-    print(g.headers["Event"].split()[0][1:], g.mainline_moves())
+    print(g.headers["Event"].split()[0][1:], g.headers["White"], g.headers["Black"], solve(g.headers["FEN"]))
 
 d()
+engine.quit()
